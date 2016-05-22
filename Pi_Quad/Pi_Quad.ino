2d676501c62a8e323a45c198a61b85f9a1b99835
lotus <dohoanghuan@gmail.com>
2016-05-23 00:01:09 +0700

    Update use new library 3.2.1

--- Pi_Quad/Pi_Quad.ino
+++ Pi_Quad/Pi_Quad.ino
@@ -1,21 +1,43 @@
+#include <AP_HAL.h>
 #include <AP_Common.h>
+#include <PID.h>
+#include <AP_Progmem.h>
 #include <AP_Math.h>
 #include <AP_Param.h>
-#include <AP_Progmem.h>
-#include <AP_ADC.h>
 #include <AP_InertialSensor.h>
-
-#include <AP_HAL.h>
-#include <AP_HAL_AVR.h>
-#include <string.h>
-#include <PID.h>
+#include <AP_ADC.h>
+#include <AP_ADC_AnalogSource.h>
+#include <AP_Baro.h>            // ArduPilot Mega Barometer Library
+#include <AP_GPS.h>
+#include <AP_AHRS.h>
+#include <AP_Compass.h>
+#include <AP_Declination.h>
+#include <AP_Airspeed.h>
+#include <GCS_MAVLink.h>
+#include <AP_Mission.h>
+#include <StorageManager.h>
+#include <AP_Terrain.h>
+#include <Filter.h>
+#include <SITL.h>
+#include <AP_Buffer.h>
+#include <AP_Notify.h>
+#include <AP_Vehicle.h>
+#include <DataFlash.h>
 #include <AP_BattMonitor.h>
+
+#include <AP_HAL_AVR.h>
+#include <AP_HAL_AVR_SITL.h>
+#include <AP_HAL_Empty.h>
 
 // ArduPilot Hardware Abstraction Layer
 const AP_HAL::HAL& hal = AP_HAL_AVR_APM2;
 
 // MPU6050 accel/gyro chip
-AP_InertialSensor_MPU6000 ins;
+AP_InertialSensor ins;
+AP_Baro_MS5611 baro(&AP_Baro_MS5611::spi);
+AP_Compass_HMC5843 compass;
+AP_GPS gps;
+AP_AHRS_DCM  ahrs(ins, baro, gps);
 
 // Battery info
 uint32_t bat_Schl;
@@ -50,7 +72,6 @@ void setup()
 {
   // Enable the motors and set at 400Hz update
   hal.rcout->set_freq(0xF, 400);
-  hal.rcout->enable_mask(0xFF);
 
   // PID Configuration
   pids[PID_PITCH_RATE].kP(0.7);
@@ -70,19 +91,15 @@ void setup()
   pids[PID_YAW_STAB].kP(10);
 
   // Turn off Barometer to avoid bus collisions
-  hal.gpio->pinMode(40, GPIO_OUTPUT);
+  hal.gpio->pinMode(40, HAL_GPIO_OUTPUT);
   hal.gpio->write(40, 1);
-  
+
   // Turn on MPU6050 - quad must be kept still as gyros will calibrate
   ins.init(AP_InertialSensor::COLD_START, 
-			 AP_InertialSensor::RATE_100HZ,
-                        NULL);
+			 AP_InertialSensor::RATE_100HZ);
 
   // initialise sensor fusion on MPU6050 chip (aka DigitalMotionProcessing/DMP)
-  hal.scheduler->suspend_timer_procs();  // stop bus collisions
-  ins.dmp_init();
-  hal.scheduler->resume_timer_procs();
-
+  ahrs.init();
 
   hal.uartA->begin(57600);   // for radios
   
@@ -92,6 +109,11 @@ void setup()
   bat_Schl = hal.scheduler->millis();
   time_TPRY = hal.scheduler->millis();
   // We're ready to go! Now over to loop()
+  hal.rcout->enable_ch(MOTOR_FL);
+  hal.rcout->enable_ch(MOTOR_FR);
+  hal.rcout->enable_ch(MOTOR_BL);
+  hal.rcout->enable_ch(MOTOR_BR);
+
   hal.console->printf("Finish Setup\n");
 }
 
@@ -115,9 +137,6 @@ uint8_t verify_chksum(char *str, char *chk) {
 void loop() 
 {
   static float yaw_target = 0;  
-  // Wait until new orientation data (normally 5ms max)
-  while (ins.num_samples_available() == 0);
-  
   static uint32_t lastPkt = 0;
  
   static int16_t channels[8] = {0,0,0,0,0,0,0,0};
@@ -145,7 +164,7 @@ void loop()
                         pdev = atof(ch);  // convert to float
                         ch = strtok(NULL, ",");  // parse pitch deviation
                         thrscl = atoi(ch);  // convert to int
-                        hal.console->printf("Setting %.2f pdev %.2f thrscl %d\n", rdev, pdev, thrscl);
+                        hal.console->printf_P(PSTR("Setting %.2f pdev %.2f thrscl %d\n"), rdev, pdev, thrscl);
                     } else if (strstr(str, "pr_rate")) {
                         ch = strtok(str, "pr_rate,"); // parse Kp
                         pids[PID_PITCH_RATE].kP(atof(ch));  // convert to float
@@ -156,7 +175,7 @@ void loop()
                         ch = strtok(NULL, ",");  // parse Kd
                         pids[PID_PITCH_RATE].kD(atof(ch));
                         pids[PID_ROLL_RATE].kD(atof(ch));
-                        hal.console->printf("PR Rate Kp %.1f Ki %.1f Kd %.1f\n", pids[PID_PITCH_RATE].kP(), pids[PID_PITCH_RATE].kI(),
+                        hal.console->printf_P(PSTR("PR Rate Kp %.1f Ki %.1f Kd %.1f\n"), pids[PID_PITCH_RATE].kP(), pids[PID_PITCH_RATE].kI(),
                                 pids[PID_PITCH_RATE].kD());
                     } else if (strstr(str, "pr_stab")) {
                         ch = strtok(str, "pr_stab,"); // parse Kp
@@ -168,7 +187,7 @@ void loop()
                         ch = strtok(NULL, ",");  // parse Kd
                         pids[PID_PITCH_STAB].kD(atof(ch));
                         pids[PID_ROLL_STAB].kD(atof(ch));
-                        hal.console->printf("PR Stab Kp %.1f Ki %.1f Kd %.1f\n", pids[PID_PITCH_STAB].kP(), pids[PID_PITCH_STAB].kI(),
+                        hal.console->printf_P(PSTR("PR Stab Kp %.1f Ki %.1f Kd %.1f\n"), pids[PID_PITCH_STAB].kP(), pids[PID_PITCH_STAB].kI(),
                                 pids[PID_PITCH_STAB].kD());
                     } else if (strstr(str, "yaw_rate")) {
                         ch = strtok(str, "yaw_rate,"); // parse Kp
@@ -177,7 +196,7 @@ void loop()
                         pids[PID_YAW_RATE].kI(atof(ch));
                         ch = strtok(NULL, ",");  // parse Kd
                         pids[PID_YAW_RATE].kD(atof(ch));
-                        hal.console->printf("Yaw Rate Kp %.1f Ki %.1f Kd %.1f\n", pids[PID_YAW_RATE].kP(), pids[PID_YAW_RATE].kI(), pids[PID_YAW_RATE].kD());
+                        hal.console->printf_P(PSTR("Yaw Rate Kp %.1f Ki %.1f Kd %.1f\n"), pids[PID_YAW_RATE].kP(), pids[PID_YAW_RATE].kI(), pids[PID_YAW_RATE].kD());
                     } else if (strstr(str, "yaw_stab")) {
                         ch = strtok(str, "yaw_stab,"); // parse Kp
                         pids[PID_YAW_STAB].kP(atof(ch));  // convert to float
@@ -185,7 +204,7 @@ void loop()
                         pids[PID_YAW_STAB].kI(atof(ch));
                         ch = strtok(NULL, ",");  // parse Kd
                         pids[PID_YAW_STAB].kD(atof(ch));
-                        hal.console->printf("Yaw Stab Kp %.1f Ki %.1f Kd %.1f\n", pids[PID_YAW_STAB].kP(), pids[PID_YAW_STAB].kI(), pids[PID_YAW_STAB].kD());
+                        hal.console->printf_P(PSTR("Yaw Stab Kp %.1f Ki %.1f Kd %.1f\n"), pids[PID_YAW_STAB].kP(), pids[PID_YAW_STAB].kI(), pids[PID_YAW_STAB].kD());
                     } else {
                         char *ch = strtok(str, ",");  // first channel
                         channels[0] = (uint16_t) strtol(ch, NULL, 10); // parse
@@ -214,7 +233,7 @@ void loop()
   if(hal.scheduler->millis() - bat_Schl > 10000) 
   {
     battery_mon.read();
-    hal.console->printf("V: %.2f R:%d%%\n",
+    hal.console->printf_P(PSTR("V: %.2f R:%d%%\n"),
                 battery_mon.voltage()+0.45,
                 battery_mon.capacity_remaining_pct());
     bat_Schl = hal.scheduler->millis();
@@ -230,17 +249,16 @@ void loop()
   rcroll = channels[0];
   
   // Ask MPU6050 for orientation. Current angle.
-  ins.update();
+  ahrs.update();
   float roll,pitch,yaw;  
-  ins.quaternion.to_euler(&roll, &pitch, &yaw);
-  roll = ToDeg(roll) + rdev;
-  pitch = ToDeg(pitch) + pdev;
-  yaw = ToDeg(yaw) ;
+  roll = ToDeg(ahrs.roll) + rdev;
+  pitch = ToDeg(ahrs.pitch) + pdev;
+  yaw = ToDeg(ahrs.yaw) ;
   
   if((hal.scheduler->millis() - time_TPRY) > 1000)
   {
     time_TPRY = hal.scheduler->millis();
-    hal.console->printf("T:%ld R:%4.2f P:%4.2f Y:%4.0f\n", rcthr,roll,pitch,yaw);
+    hal.console->printf_P(PSTR("T:%ld R:%4.2f P:%4.2f Y:%4.0f\n"), rcthr,roll,pitch,yaw);
   }
   
   // Ask MPU6050 for gyro data. Rotational velocity
@@ -250,9 +268,9 @@ void loop()
   // Do the magic
   if(rcthr > RC_THR_MIN + 50) {  // Throttle raised, turn on stabilization.
     // Stabilize PIDS
-    float pitch_stab_output = constrain(pids[PID_PITCH_STAB].get_pid((float)rcpit - pitch, 1), -250, 250); // error = desired_input - real_reading
-    float roll_stab_output = constrain(pids[PID_ROLL_STAB].get_pid((float)rcroll - roll, 1), -250, 250);
-    float yaw_stab_output = constrain(pids[PID_YAW_STAB].get_pid(wrap_180(yaw_target - yaw), 1), -360, 360);
+    float pitch_stab_output = constrain_float(pids[PID_PITCH_STAB].get_pid((float)rcpit - pitch, 1), -250, 250); // error = desired_input - real_reading
+    float roll_stab_output = constrain_float(pids[PID_ROLL_STAB].get_pid((float)rcroll - roll, 1), -250, 250);
+    float yaw_stab_output = constrain_float(pids[PID_YAW_STAB].get_pid(wrap_180(yaw_target - yaw), 1), -360, 360);
   
     // is pilot asking for yaw change - if so feed directly to rate pid (overwriting yaw stab output)
     if(abs(rcyaw ) > 5) {
@@ -261,9 +279,9 @@ void loop()
     }
     
     // rate PIDS
-    long pitch_output =  (long) constrain(pids[PID_PITCH_RATE].get_pid(pitch_stab_output - gyroPitch, 1), - 500, 500);  
-    long roll_output =  (long) constrain(pids[PID_ROLL_RATE].get_pid(roll_stab_output - gyroRoll, 1), -500, 500);  
-    long yaw_output =  (long) constrain(pids[PID_ROLL_RATE].get_pid(yaw_stab_output - gyroYaw, 1), -500, 500);  
+    long pitch_output =  (long) constrain_int32(pids[PID_PITCH_RATE].get_pid(pitch_stab_output - gyroPitch, 1), - 500, 500);
+    long roll_output =  (long) constrain_int32(pids[PID_ROLL_RATE].get_pid(roll_stab_output - gyroRoll, 1), -500, 500);
+    long yaw_output =  (long) constrain_int32(pids[PID_ROLL_RATE].get_pid(yaw_stab_output - gyroYaw, 1), -500, 500);
 
     // mix pid outputs and send to the motors.
     hal.rcout->write(MOTOR_FL, rcthr + roll_output + pitch_output - yaw_output);
